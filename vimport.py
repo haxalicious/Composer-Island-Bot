3b7a1366f389b96c3d13e0501a35af505677ce33
haxalicious <26352208+haxalicious@users.noreply.github.com>
2023-01-18 22:46:06 -0600

    Change video script to use held notes instead of note spam for hopefully less lag
    Signed-off-by: haxalicious <26352208+haxalicious@users.noreply.github.com>

--- vimport.py
+++ vimport.py
@@ -1,16 +1,27 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 from PIL import Image, ImageSequence
 import numpy as np
 import pyautogui
 import random
 import time
 
+def click(x, y):
+    pyautogui.moveTo(x, y)
+    pyautogui.click()
+    print("click {0}, {1}".format(str(x), str(y)))
+
+def drag(x0, x1, y):
+    pyautogui.moveTo(x0, y)
+    pyautogui.click()
+    time.sleep(random.uniform(0.03, 0.06))
+    pyautogui.dragTo(x1, y, random.uniform(0.15, 0.25), pyautogui.easeOutQuad, button='left')
+
 def compose(x, y):
     # Config values
-    file = 'example.gif'  # Change this to your GIF name
+    file = 'example.gif'  # Path to GIF
     xstart = 119  # X coord of the farthest right point inside the leftmost note
-    xincrement = 122  # Pixel count between notes
-    xmax = xstart + xincrement * 13  # Max note coord, likely no need to change
+    xstep = 122  # Pixel count between notes
+    xmax = xstart + xstep * 13  # Max note coord, likely no need to change
     y0 = 369  # Y coord for low B
     y1 = 536  # Y coord for low B
     y2 = 600  # Y coord for high F
@@ -27,57 +38,77 @@ def compose(x, y):
     successbutton_x = 912  # X and Y coords for the checkbox on the save successful dialog
     successbutton_y = 687
 
-    img = Image.open(file)  # Change this to your GIF file
+    # Generate an array from GIF, extract RGB data for a single pixel
+    img = Image.open(file)
     frames = np.array([np.array(frame.copy().convert('RGB').getdata(), dtype=np.uint8).reshape(frame.size[1], frame.size[0], 3) for
          frame in ImageSequence.Iterator(img)]).transpose(1, 2, 0, 3)
     frames = np.flip(frames, 0)
-    pixel = frames[y][x]  # Generate an array from GIF, extract RGB data for a single pixel
+    pixel = frames[y][x]
 
-    x = xstart + xincrement * 4
-    pyautogui.moveTo(accidentalbutton_x, accidentalbutton_y)
-    pyautogui.click()
-    print("click {0}, {1}".format(str(accidentalbutton_x), str(accidentalbutton_y)))
-    time.sleep(random.uniform(0.1, 0.2))
+    x = xstart + xstep * 4  # Initialize vars
+    xdrag = x
+
+    click(accidentalbutton_x, accidentalbutton_y)
+    time.sleep(random.uniform(0.05, 0.1))
+    click(accidentalbutton_x, accidentalbutton_y)
+    time.sleep(random.uniform(0.075, 0.15))
 
     for i in range(512):
         print("processing frame {0}".format(str(i)))
-        if i == 498:
-            pyautogui.moveTo(nextbutton_x, nextbutton_y)
-            pyautogui.click()
-            print("click {0}, {1}".format(str(nextbutton_x), str(nextbutton_y)))
-            x = xstart
-            time.sleep(random.uniform(0.1, 0.2))
-        if pixel[i][0] == color_white:
-            y = y0
-        elif pixel[i][0] == color_black:
-            y = y2
-        else:
-            y = y1
-        pyautogui.moveTo(x, y)
-        pyautogui.click()
-        print("click {0}, {1}".format(str(x), str(y)))
-        time.sleep(random.uniform(0.02, 0.03))
-        if x >= xmax:
-            if i == 511:
-                pyautogui.moveTo(exitbutton_x, exitbutton_y)
-                pyautogui.click()
-                print("click {0}, {1}".format(str(exitbutton_x), str(exitbutton_y)))
-                time.sleep(random.uniform(0.5, 0.7))
-                pyautogui.moveTo(confirmbutton_x, confirmbutton_y)
-                pyautogui.click()
-                print("click {0}, {1}".format(str(confirmbutton_x), str(confirmbutton_y)))
-                time.sleep(random.uniform(0.7, 0.9))
-                pyautogui.moveTo(successbutton_x, successbutton_y)
-                pyautogui.click()
-                print("click {0}, {1}".format(str(successbutton_x), str(successbutton_y)))
+
+        if i == 498:  # Workaround for end
+            if xdrag < x - xstep:
+                drag(xdrag, x - xstep, y)
             else:
-                pyautogui.moveTo(nextbutton_x, nextbutton_y)
-                pyautogui.click()
-                print("click {0}, {1}".format(str(nextbutton_x), str(nextbutton_y)))
-                x = xstart
-                time.sleep(random.uniform(0.1, 0.2))
+                click(xdrag, y)
+                time.sleep(random.uniform(0.025, 0.05))
+            click(nextbutton_x, nextbutton_y)
+            x = xstart
+            xdrag = xstart
+            time.sleep(random.uniform(0.075, 0.15))
+
+        if pixel[i][0] == color_white:  # Set Y value
+            ynext = y0
+        elif pixel[i][0] == color_black:
+            ynext = y2
         else:
-            x += xincrement
+            ynext = y1
+        if i == 0:
+            y = ynext
+
+        if y != ynext:  # Input note
+            if xdrag == x - xstep:
+                click(xdrag, y)
+            else:
+                drag(xdrag, x - xstep, y)
+            xdrag = x
+            y = ynext
+
+        if x >= xmax:
+            if i == 511:  # Input last note, save, and exit
+                if xdrag < x:
+                    drag(xdrag, x, y)
+                else:
+                    click(xdrag, y)
+                    time.sleep(random.uniform(0.025, 0.05))
+                click(exitbutton_x, exitbutton_y)
+                time.sleep(random.uniform(0.5, 0.7))
+                click(confirmbutton_x, confirmbutton_y)
+                time.sleep(random.uniform(0.8, 1))
+                click(successbutton_x, successbutton_y)
+
+            else:  # Input note and click next button
+                if xdrag < x:
+                    drag(xdrag, x, y)
+                else:
+                    click(xdrag, y)
+                    time.sleep(random.uniform(0.025, 0.05))
+                click(nextbutton_x, nextbutton_y)
+                x = xstart
+                xdrag = xstart
+                time.sleep(random.uniform(0.075, 0.15))
+        else:
+            x += xstep
 
 
 if __name__ == '__main__':
